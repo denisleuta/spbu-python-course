cb81194d22efcc95bba52a8699e184528b6f7d6d
denis <denis@DESKTOP-8AJ8D96>
2024-09-30 14:16:51 +0300

    Fix bugs

--- project/matrix_operations.py
+++ project/matrix_operations.py
@@ -1,13 +1,16 @@
 import math
 from typing import List
 
+
 # Сложенние матриц
 def matrix_addition(m1: List[List[float]], m2: List[List[float]]) -> List[List[float]]:
     return [[x + y for x, y in zip(row1, row2)] for row1, row2 in zip(m1, m2)]
 
+
 # Умножение матриц
 def matrix_multiply(m1: List[List[float]], m2: List[List[float]]) -> List[List[float]]:
     return [[sum(x * y for x, y in zip(row, col)) for col in zip(*m2)] for row in m1]
+
 
 # Транспонирование матриц
 def transpose_matrix(m: List[List[float]]) -> List[List[float]]:
--- project/vector_operations.py
+++ project/vector_operations.py
@@ -1,13 +1,16 @@
 import math
 from typing import List
 
+
 # Скалярное умножение
 def scalar_multyply(v1: List[float], v2: List[float]) -> float:
     return sum(x * y for x, y in zip(v1, v2))
 
+
 # Длина векторов
 def vector_length(v: List[float]) -> float:
     return math.sqrt(scalar_multyply(v, v))
+
 
 # Угол между векторами
 def angle_vectors(v1: List[float], v2: List[float]) -> float:
