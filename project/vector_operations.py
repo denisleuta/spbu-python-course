ac7deeb5e989fc7feff1123efa6881cc067c94b4
denis <denis@DESKTOP-8AJ8D96>
2024-09-30 14:12:24 +0300

    добавление комментариев

--- project/vector_operations.py
+++ project/vector_operations.py
@@ -1,14 +1,14 @@
 import math
 from typing import List
 
-
+# Скалярное умножение
 def scalar_multyply(v1: List[float], v2: List[float]) -> float:
     return sum(x * y for x, y in zip(v1, v2))
 
-
+# Длина векторов
 def vector_length(v: List[float]) -> float:
     return math.sqrt(scalar_multyply(v, v))
 
-
+# Угол между векторами
 def angle_vectors(v1: List[float], v2: List[float]) -> float:
     return math.acos(scalar_multyply(v1, v2) / (vector_length(v1) * vector_length(v2)))
