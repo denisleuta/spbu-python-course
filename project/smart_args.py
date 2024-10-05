4d056ce9751afc53314b821718abe086e5b83e80
denis <denis@DESKTOP-8AJ8D96>
2024-10-05 20:49:58 +0300

    Run test

--- project/smart_args.py
+++ project/smart_args.py
@@ -13,21 +13,18 @@ class Evaluated:
         self.func = func
 
 
+import inspect
+from functools import wraps
+
+
 def smart_args(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         func_spec = inspect.getfullargspec(func)
 
-        for name, default in func_spec.kwonlydefaults.items():
-            if name not in kwargs:
-                if isinstance(default, Isolated):
-                    assert (
-                        name in kwargs
-                    ), f"Isolated requires a passed argument for{name}"
-                    kwargs[name] = copy.deepcopy(kwargs[name])
-                elif isinstance(default, Evaluated):
-                    kwargs[name] = default.func()
-                else:
+        if func_spec.kwonlydefaults:
+            for name, default in func_spec.kwonlydefaults.items():
+                if name not in kwargs:
                     kwargs[name] = default
 
         return func(*args, **kwargs)
